2dde8d0468de097d9716ed3f4ad7b19d1e10e4d6
mentadp <67691873+mentadp@users.noreply.github.com>
2021-11-28 20:52:30 +0700

    Add files via upload

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php 
+	$hostname = 'localhost';
+	$username = 'smkn1pa_perdame';
+	$password = '6JXwuNLs3hyd';
+	$dbname   = 'smkn1pa_perdame';
+
+	$conn = mysqli_connect($hostname, $username, $password, $dbname) or die ('Gagal terhubung ke database');
